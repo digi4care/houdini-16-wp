e05bb220e574f99841eb747aa4112da1ec5543b9
jycouet <jycouet@gmail.com>
2022-08-26 00:55:12 +0200

    :zap: NEW: first query example

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
-<script  lang="ts">
-	import client from '../client'
+<script lang="ts">
+	import client from '../client';
 
-	client.init()
+	client.init();
 </script>
+
+<h1>Welcome to Houdini</h1>
+<p>
+	Visit <a href="https://docs-next-kohl.vercel.app/">https://docs-next-kohl.vercel.app/</a> to read the
+	documentation
+</p>
+
+<hr />
+
+<a href="/">Home</a>
+<a href="/about">About</a>
+
+<hr />
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: ({ Continents } = data);
+</script>
+
+<h2>Home</h2>
+
+This is the home page, with the list of continents
+
+{#each $Continents?.data?.continents ?? [] as { code, name }}
+	<div>{code} - {name}</div>
+{/each}
