41a33ea79f6864be2279d3630ef7ec6e1fb36370
Chris Engelhard <c.engelhard@digi4care.nl>
2022-09-09 22:19:09 +0200

    Load WebsiteStore query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,11 @@
 	import client from '../client';
 
 	client.init();
+
+	export let data;
+	$: ({ Website } = data);
+
+	$: console.log($Website);
 </script>
-
-<h1>Welcome to Houdini</h1>
-<p>
-	Visit <a href="https://docs-next-kohl.vercel.app/">https://docs-next-kohl.vercel.app/</a> to read the
-	documentation
-</p>
-
-<hr />
-
-<a href="/">Home</a>
-<a href="/about">About</a>
-
-<hr />
 
 <slot />
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,48 +1,9 @@
-import { graphql } from '$houdini'
+import WebsiteStore from '$houdini/stores/Website'
 
-export const houdini_load = graphql`
-query Websitev1 {
-	generalSettings {
-		description
-		language
-		title
-		url
+export function load({ event }) {
+	WebsiteStore.fetch({ event })
+
+	return {
+		Website: WebsiteStore
 	}
-	readingSettings {
-		showOnFront
-		postsPerPage
-		pageOnFront
-		pageForPosts
-	}
-	headerSettings: getHeader {
-		favicon
-		siteLogoUrl
-		siteTagLine
-		siteTitle
-	}
-	footerSettings: getFooter {
-		copyrightText
-		sidebarOne
-		sidebarTwo
-	}
-	getHeaderMenu: menuItems(where: { location: HCMS_MENU_HEADER }, first: 99) {
-		nodes {
-			...menuFieldItems
-		}
-	}
-	getFooterMenu: menuItems(where: { location: HCMS_MENU_FOOTER }, first: 99) {
-		nodes {
-			...menuFieldItems
-		}
-	}
-	getLoggedInMenu: menuItems(where: { location: HCMS_MENU_LOGGEDIN }, first: 99) {
-		nodes {
-			...menuFieldItems
-		}
-	}
-	getLoggedOutMenu: menuItems(where: { location: HCMS_MENU_LOGGEDOUT }, first: 99) {
-		nodes {
-			...menuFieldItems
-		}
-	}
-}`
+}
