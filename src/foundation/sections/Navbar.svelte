27e6ea5020301a941645f2fe7d13bb4d15118aa5
Chris Engelhard <c.engelhard@digi4care.nl>
2022-09-09 22:18:45 +0200

    navbar fragment added

---
+++ src/foundation/sections/Navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { fragment, graphql } from '$houdini';
+
+	export let user;
+
+	$: data = fragment(
+		user,
+		graphql`
+			fragment menuFieldItems on MenuItem {
+				id
+				parentId
+				label
+				cssClasses
+				description
+				linkRelationship
+				order
+				path
+				target
+				title
+				uri
+				url
+			}
+		`
+	);
+</script>
