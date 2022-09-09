3dd30b7539d384208d226544eb7842346635b4a1
Chris Engelhard <c.engelhard@digi4care.nl>
2022-09-10 00:16:25 +0200

    Flowbite navbar

--- src/foundation/sections/Navbar.svelte
+++ src/foundation/sections/Navbar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 	import { fragment, graphql } from '$houdini';
 
-	export let user;
-
+	export let nodes;
 	$: data = fragment(
-		user,
+		nodes,
 		graphql`
 			fragment menuFieldItems on MenuItem {
 				id
@@ -22,4 +23,26 @@
 			}
 		`
 	);
+	// $: console.log(data);
 </script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<img
+			src="https://flowbite.com/docs/images/logo.svg"
+			class="mr-3 h-6 sm:h-9"
+			alt="Flowbite Logo"
+		/>
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Flowbite
+		</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		{#if nodes}
+			{#each nodes as link}
+				<NavLi href={link.path} active={$page.url.pathname === link.path}>{link.label}</NavLi>
+			{/each}
+		{/if}
+	</NavUl>
+</Navbar>
